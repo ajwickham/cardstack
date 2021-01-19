88a59e97795b38e6bfd4e76d9e636c9b7492c354
Alan Wickham <ajwickham@yahoo.com>
2021-01-19 16:10:29 +0000

    import and export sorted out.  Still unclear why nesting commands doesn't work - I guess if its exported you can't access it.  But it does now work

--- src/constructors.js
+++ src/constructors.js
@@ -1,18 +1,11 @@
-//export 
-function Pairallocation() {
+export function Pairs() {
     this.letters= [];
     this.ordinal= ["#first","#second","#third","#fourth","#fifth","#sixth","#seventh","#eigth","#ninth","#tenth"];
-  }
-
-//export 
-function Pairguess(){
     this.guess = [];
     this.result = [];
-  };
-  
+}
 
-
-Pairallocation.prototype.allocatePairs =function() {
+Pairs.prototype.allocatePairs =function() {
   let letterslist = ["A","B","C","D","E","A","B","C","D","E"];
   let x;
   let y;
@@ -20,46 +13,60 @@ Pairallocation.prototype.allocatePairs =function() {
     y = Math.round(Math.random()*(i));
     x= letterslist[y];
     letterslist.splice(y,1);
-    this.letters.push(x);
-    
+    this.letters.push(x);   
   }
   this.letters.push(letterslist[0]);
 };
 
-Pairallocation.prototype.correctFlipSub = function(x,y) {
-  if( pairs.letters[x]===pairs.letters[y] ) {
-    $(pairs.ordinal[y]).addClass("flip-card");
-    $(pairs.ordinal[y]).removeClass("flip-card2");
-    $(pairs.ordinal[x]).addClass("flip-card");
-    $(pairs.ordinal[x]).removeClass("flip-card2");
-  }
+Pairs.prototype.pairLetter = function(x,y) {
+  return this.letters[x,y];
 }
-let count;
-Pairguess.prototype.addGuess = function(position) {
-debugger
+
+
+Pairs.prototype.addGuess = function(position,pairs) {
   this.guess.push(position);
-  count = this.guess.length-1
+  var count = this.guess.length-1
   if(count >= 1) {
     if(this.guess[count] === this.guess[count-1]) {
-    alert("You just flipped that one!")
+    alert("You just flipped that one!");
     }
     else {
-      if(pairs.letters[(this.guess[count])]===pairs.letters[(this.guess[count-1])]) {
+      if(this.letters[(this.guess[count])]===this.letters[(this.guess[count-1])]) {
         this.result.push("correct");
       }
     }
-    document.getElementById("r2").innerHTML = this.result.length
-    
-    
+    document.getElementById("r2").innerHTML = this.result.length   
   }
   document.getElementById("g2").innerHTML = this.guess.length;
 };
 
-Pairguess.prototype.correctFlip = function() {  //Program keeps missing correct cards because of time delay.  This function checks all cards every time to make sure.
-  for(let k=1; k <this.guess.length; k++) {
-    if((this.guess[k])!= (this.guess[k-1])) {
-      pairs.correctFlipSub(this.guess[k],this.guess[k-1])
+
+Pairs.prototype.firstFlip = function() {
+  let card = this.guess[((this.guess.length)-1)];
+  let previousCard = this.guess[((this.guess.length)-2)];
+  document.getElementById(card).innerHTML = this.letters[card];
+  let position = this.ordinal[card];
+  if(card != previousCard) {
+    $(position).addClass("flip-card");                            //flip card to show letter
+    $(position).removeClass("flip-card2");
+    setTimeout(function(){ 
+      $(position).addClass("flip-card2");                         //after timeout flip card to show blank side
+      $(position).removeClass("flip-card"); 
+    }, 5000);
+  } 
+}
+
+Pairs.prototype.flipBack =  function () {
+  for(let k=1; k <(this.guess.length); k++) {
+    let x = this.guess[k];
+    let y = this.guess[k-1];
+    if(this.letters[x]===this.letters[y]) {               //if the pair matches flip both cards to show letters
+      $(this.ordinal[y]).addClass("flip-card");
+      $(this.ordinal[y]).removeClass("flip-card2");
+      $(this.ordinal[x]).addClass("flip-card");
+      $(this.ordinal[x]).removeClass("flip-card2");
     }
-  }
-};
+  } 
+}
+
 
--- src/main.js
+++ src/main.js
@@ -1,125 +1,68 @@
-//import {Pairallocation,Pairguess} from './constructors.js';
-//const pairs = new Pairallocation;
+import {Pairs} from './constructors.js';
+let pairs = new Pairs;
 
-
-let pairs = new Pairallocation;
 
 pairs.allocatePairs();
 
-/*function Pairguess(){
-  this.guess = [];
-  this.result = [];
-};*/
-
-let guesses = new Pairguess;
-/*Pairguess.prototype.addGuess = function(position) {
-  count = count +1
-  this.guess.push(position);
-  if(count >= 1) {
-    if(this.guess[count] === this.guess[count-1]) {
-    alert("You just flipped that one!")
-    }
-    else {
-      if(pairs.letters[(this.guess[count])]===pairs.letters[(this.guess[count-1])]) {
-        this.result.push("correct");
-      }
-    }
-    document.getElementById("r2").innerHTML = this.result.length
-    document.getElementById("g2").innerHTML = this.guess.length;
-    
-  }
-};*/
-
-
-//This is the main function, what happens when you click the stack
-
-function cardFlip() {
- 
-  let card = guesses.guess[((guesses.guess.length)-1)];
-  let previousCard = guesses.guess[((guesses.guess.length)-2)];
-  document.getElementById(card).innerHTML = pairs.letters[card];
-  let position = pairs.ordinal[card];
-    
-  if(card != previousCard) {
-    $(position).addClass("flip-card");
-    $(position).removeClass("flip-card2");
-    startTimeOut(position);
-  }
-};
-
-let timeOut;
-function startTimeOut(position){
-  timeOut = setTimeout(function(){ 
-    $(position).addClass("flip-card2");
-    $(position).removeClass("flip-card"); 
-    guesses.correctFlip(); 
+let turnCard = function() {
+  pairs.firstFlip();
+  setTimeout(function(){ 
+    pairs.flipBack();
   }, 5000); 
+}
 
-};
-/*
-Pairguess.prototype.correctFlip = function() {  //Program keeps missing correct cards because of time delay.  This function checks all cards every time to make sure.
-  for(let k=1; k <this.guess.length; k++) {
-    if((this.guess[k])!= (this.guess[k-1])) {
-      if( pairs.letters[(this.guess[k-1])]===pairs.letters[(this.guess[k])] ) {
-        $(pairs.ordinal[(this.guess[k-1])]).addClass("flip-card");
-        $(pairs.ordinal[(this.guess[k-1])]).removeClass("flip-card2");
-        $(pairs.ordinal[(this.guess[k])]).addClass("flip-card");
-        $(pairs.ordinal[(this.guess[k])]).removeClass("flip-card2");
-      }
-    }
-  }
-};*/
+
+
 
 $(document).ready(function() {
 
-  $("#first").click(function() { 
-    guesses.addGuess(0,count);
-    cardFlip();
+  $("#first").click(function() {
+    pairs.addGuess(0);
+    turnCard();
   });
   $("#second").click(function() { 
-    guesses.addGuess(1,count);
-    cardFlip();  
+    pairs.addGuess(1);
+    turnCard();
   });
 
   $("#third").click(function() { 
-    document.getElementById("2").innerHTML = pairs.letters[2];
-    guesses.addGuess(2,count);
-    cardFlip();
+    pairs.addGuess(2);
+    turnCard();
   });
 
   $("#fourth").click(function() { 
-    guesses.addGuess(3,count);
-    cardFlip();
+    pairs.addGuess(3);
+    turnCard();
   });
 
   $("#fifth").click(function() { 
-    guesses.addGuess(4,count);
-    cardFlip();   
+    pairs.addGuess(4);
+    turnCard();  
   });
 
   $("#sixth").click(function() { 
-    guesses.addGuess(5,count);
-    cardFlip();
+    pairs.addGuess(5);
+    turnCard();  
   });
 
   $("#seventh").click(function() { 
-    guesses.addGuess(6,count);
-    cardFlip(); 
+    pairs.addGuess(6);
+    turnCard();   
   });
 
   $("#eigth").click(function() { 
-    guesses.addGuess(7,count);
-    cardFlip();
+    pairs.addGuess(7);
+    turnCard();  
     });
   
   $("#ninth").click(function() { 
-    guesses.addGuess(8,count);
-    cardFlip(); 
+    pairs.addGuess(8);
+    turnCard();  
   });
 
   $("#tenth").click(function() { 
-    guesses.addGuess(9,count);
-    cardFlip(); 
+    pairs.addGuess(9);
+    turnCard();   
   });
 });
 
